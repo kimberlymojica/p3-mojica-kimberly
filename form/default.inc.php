d3ae9448433d76b9a5053f2b7c8d0aca252bda4a
Kimberly Mojica <kimberly.mojica@ufl.edu>
2021-04-23 14:26:30 -0700

    Added css to default page

--- form/default.inc.php
+++ form/default.inc.php
@@ -1,2 +1,87 @@
-<h1>Hello</h1>
-<p>Form processor by <a href="http://www.inventpartners.com">Invent Partners Web Design York</a>.</p>
+<style type="text/css" >
+
+*{
+  padding: 0;
+  margin: 0;
+  border: 0;
+}
+
+/*----------------------------------------------
+
+ Logo
+
+-------------------------------------------------*/
+#logo {
+  margin: 5px 450px;
+}
+
+/*----------------------------------------------
+
+Nav Bar
+
+-------------------------------------------------*/
+header{
+ margin: 0 auto;
+ text-align: center;
+
+}
+
+header nav li{
+  display: inline-block;
+  margin: 20px auto;
+  color: black;
+}
+
+header nav a{
+  padding: 15px 60px;
+  text-transform: uppercase;
+  color: black;
+  text-decoration: none;
+  border-top: 2px solid #000;
+  border-bottom: 2px solid #000;
+  margin: -5px;
+  font-weight: bold;
+}
+
+nav a:visited {
+  color: #77917B;
+}
+
+nav a:hover {
+  color: #97636D;
+}
+
+nav a:active {
+  color: #D0ABAE;
+}
+/*----------------------------------------------
+
+Index Body
+
+-------------------------------------------------*/
+
+.container {
+  width: 1350px;
+  height: auto;
+  background-color: pink;
+  margin: 0px auto;
+  position: absolute;
+}
+
+h1 {
+  font-family: 'sewstain',cursive;
+  font-size: 46px;
+  text-shadow: 1px 1px 2px white, 0 0 1em grey, 0 0 0.2em grey;
+  margin: 50px 25px;
+  width: 200px;
+  float: right;
+  color: black;
+  padding: 0 370px;
+}
+
+</style>
+
+<div class="container"
+<h1>Whoops! Something went wrong...</h1>
+<p>Please try again.</a>.</p>
+</div>
