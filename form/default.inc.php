98523f2d28db97ee50d8bb3fafa4ca4b46d78452
Kimberly Mojica <kimberly.mojica@ufl.edu>
2021-04-24 14:45:58 -0700

    Added default css

--- form/default.inc.php
+++ form/default.inc.php
@@ -1,2 +1,85 @@
-<h5>Hello</h5>
-<p>Whoops! Something Went Wrong... <a href="http://www.inventpartners.com">Invent Partners Web Design York</a>.</p>
+<link rel="icon"  type="image/ico" href="images/favicon.ico">
+<link rel="icon" type="img/png" href="images/favicon.png">
+<link rel="stylesheet" href="css/styles.css">
+
+<style type="text/css">
+*{
+  padding: 0;
+  margin: 0;
+  border: 0;
+}
+
+/*----------------------------------------------
+
+ Logo
+
+-------------------------------------------------*/
+#logo {
+  margin: 5px 450px;
+}
+
+/*----------------------------------------------
+
+Nav Bar
+
+-------------------------------------------------*/
+header{
+ margin: 0 auto;
+ text-align: center;
+
+}
+
+header nav li{
+  display: inline-block;
+  margin: 20px auto;
+  color: black;
+}
+
+header nav a{
+  padding: 15px 60px;
+  text-transform: uppercase;
+  color: black;
+  text-decoration: none;
+  border-top: 2px solid #000;
+  border-bottom: 2px solid #000;
+  margin: -5px;
+  font-weight: bold;
+}
+
+header nav a:visited {
+  color: #77917B;
+}
+
+header nav a:hover {
+  color: #97636D;
+}
+
+header nav a:active {
+  color: #D0ABAE;
+}
+.contain{
+
+}
+</style>
+
+<div id="logo">
+ <img src="images/logo.png" alt="logo" />
+</div>
+
+<div class="contain">
+
+</div>
+<header>
+  <nav>
+    <ul>
+      <li><a href="index.html">HOME</a></li>
+      <li><a href="lips.html">LIPS</a></li>
+      <li><a href="eyes.html">EYES</a></li>
+      <li><a href="contact.html">CONTACT</a></li>
+    </ul>
+  </nav>
+</header>
+
+<h5>Whoops! Something Went Wrong...</h5>
+<p>Please try again.</p>
+</div>
